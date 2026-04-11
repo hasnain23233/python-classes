320589dd4be1a3f5f181bc633a0a7e18bad1b35a
Hasnain Khushi <hasnainkhushii@gmail.com>
2026-04-11 23:56:04 +0500

    In this class we learn about

--- firstClass/index.py
+++ firstClass/index.py
@@ -1,2 +1,16 @@
 print("This is a python programming langage :)")
-print("Hasnain khushi is a programmer.")
+print("Hasnain khushi is a programmer." * 10)
+
+id = 39
+print(id)
+nameDev = "Hasnain khushi"
+language = "JavaScript and python"
+id = 38
+print(id)
+
+
+pythonProgramming = input("Enter you projramming language")
+age = input("Enter your age")
+age = int(age)
+
+print ("You love " , pythonProgramming , " and your age is " , age)
