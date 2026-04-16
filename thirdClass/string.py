3d2ba447ce5eb7affdc2f3a3409e33ee89b58d56
Hasnain Khushi <hasnainkhushii@gmail.com>
2026-04-17 02:32:57 +0500

    working on the string

---
+++ thirdClass/string.py
@@ -0,0 +1,16 @@
+str1 = 'Python is a easy programming'
+str2 = "Hasnain" 
+str3 = '''This is a python course'''
+
+print(str1)
+print(str2)
+print(str3)
+
+print(str1 + str2)
+
+print(len(str2 + str3))
+
+# indexing
+print(str1[0])
+print(str2[1])
+print(str3[2])
