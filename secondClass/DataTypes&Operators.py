5a102486cd99cbca0c41c84eaec02927cf6cce80
Hasnain Khushi <hasnainkhushii@gmail.com>
2026-04-17 01:43:51 +0500

    working on the python

---
+++ secondClass/DataTypes&Operators.py
@@ -0,0 +1,34 @@
+# print("In this class we learn about python datatypes and operators")
+
+# name = "Akash"
+# number = 39
+# disk = 93.4
+# login = True
+
+# print(type(name))
+# print(type(number))
+# print(type(disk))
+# print(type(login))
+
+
+# userData = input('Enter your name of the student ')
+# print('The name of the student is :) ' , userData)
+# print("The data type of the input is :) " , type(userData))
+
+
+
+# #print two number in python programming 
+
+# num1 = input ("Enter your first number to add :) ")
+# num2 = input ("Enter your second number to add :) ")
+
+# add = int(num1) + int(num2)
+# print("The totail number of the two decimals is :) " ,add)
+
+
+
+use = input("Enter you name here plx :) ")
+age = int(input("Enter your age here plx :) "))
+job = input("Enter you job description :) ")
+ 
+print("So you name is " , use , "and your age is " , age , ". Your job is " , job)
